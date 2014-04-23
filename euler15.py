6ba1680d1f4a3e5955e538d697327be90e14e276
Lingqiang Kong <konglq@gmail.com>
2014-04-23 06:29:35 +0000

    solved euler15

---
+++ euler15.py
@@ -0,0 +1,27 @@
+def euler15():
+    # Problem:
+    """
+    Starting in the top left corner of a 2x2 grid, 
+    and only being able to move to the right and down, 
+    there are exactly 6 routes to the bottom right corner.
+
+    How many such routes are there through a 20x20 grid?
+    """
+
+    # Solve:
+    N = 21
+    routes = [[None] * N] * N
+    for i in range(N):
+        routes[i][N-1] = 1
+        routes[N-1][i] = 1
+
+    for i in range(N-2,-1,-1):
+        for j in range(N-2,-1,-1):
+            routes[i][j] = routes[i+1][j] + routes[i][j+1]
+            print i,j,routes[i][j]
+
+    return routes
+
+euler15() #137846528820
+
+    
